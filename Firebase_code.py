74853801c4165084a6c8ebf4d46acc8ccfcf4b6f
KaRaNNK0710 <karankulkarni0710@gmail.com>
2025-05-19 23:50:08 +0530

    changes in firebase.py

--- Firebase_code.py
+++ Firebase_code.py
@@ -8,6 +8,7 @@ import logging
 from datetime import datetime, timezone, timedelta
 import re
 import json
+import os
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -15,8 +16,19 @@ logger = logging.getLogger(__name__)
 class BankingSystem:
     def __init__(self):
         if not firebase_admin._apps:
-            cred_dict = json.loads(st.secrets["GOOGLE_CREDENTIALS"])
-            cred_dict["private_key"] = cred_dict["private_key"].replace("\\n", "\n")
+            cred_dict = {
+                "type": os.environ["FIREBASE_TYPE"],
+                "project_id": os.environ["FIREBASE_PROJECT_ID"],
+                "private_key_id": os.environ["FIREBASE_PRIVATE_KEY_ID"],
+                "private_key": os.environ["FIREBASE_PRIVATE_KEY"].replace("\\n", "\n"),
+                "client_email": os.environ["FIREBASE_CLIENT_EMAIL"],
+                "client_id": os.environ["FIREBASE_CLIENT_ID"],
+                "auth_uri": os.environ["FIREBASE_AUTH_URI"],
+                "token_uri": os.environ["FIREBASE_TOKEN_URI"],
+                "auth_provider_x509_cert_url": os.environ["FIREBASE_AUTH_PROVIDER_X509_CERT_URL"],
+                "client_x509_cert_url": os.environ["FIREBASE_CLIENT_X509_CERT_URL"],
+                "universe_domain": os.environ["FIREBASE_UNIVERSE_DOMAIN"]
+            }
             cred = credentials.Certificate(cred_dict)
             firebase_admin.initialize_app(cred)
         self.db = firestore.client()
